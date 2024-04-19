2e79bbce942aa8bbe0aee2e6da93e76de9533c1f
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-04-19 23:44:10 +0530

    Day 3

---
+++ Arrays/creating_array.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+
+  /*
+       1. datatype arrayName[]=new datatype[size];
+       2. length of array=arr.length()
+         */
+
+public class creating_array {
+    public static void main(String[] args) {
+        int marks[]=new int[50];
+        int num[]={1,2,3};
+        String name[]={"Pulkit","Nimrat","Geet","Sehaj"};
+        char arr[]={'a','b','c'};
+        float arr1[]={2.4f,3.5f};
+        System.out.println("Length of name array is: "+ name.length);
+        
+    }
+    
+}
