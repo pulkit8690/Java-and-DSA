37fb7bc84565080a90b2f63b10fb2621a5265f7a
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-06-26 16:46:17 +0530

    26 June 2024

---
+++ Linked List 2/LinkedList3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LinkedList3 {
+    public static void main(String[] args) {
+        //Creation
+        LinkedList<Integer> ll=new LinkedList<>();
+        //Add ELement
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        //Print
+        System.out.println(ll);
+        //Remove Element
+        ll.removeLast();
+        System.out.println(ll);
+        ll.removeFirst();
+        System.out.println(ll);
+    }
+}
