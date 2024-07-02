a667978d9f6cf6d99d83856a9f477af753d4bf35
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-07-02 07:55:07 +0530

    ValidParenthesis

---
+++ Stacks/ValidParenthesis.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class ValidParenthesis {
+    public static boolean isValidParenthesis(String s)
+    {
+        Stack<Character> t= new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch= s.charAt(i);
+            if(ch=='(' ||ch=='{' ||ch=='[')
+            {
+                t.push(ch);
+            }
+            else
+            {
+                if(t.isEmpty())
+                {
+                    return false;
+                }
+                if((t.peek()=='(' && ch==')') ||(t.peek()=='[' && ch==']') ||(t.peek()=='{' && ch=='}'))
+                {
+                    t.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(t.isEmpty())
+        {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String s1="({[]})";
+        String s2="[{)]";
+        String s3="[()]{}";
+        System.out.println(isValidParenthesis(s1));
+        System.out.println(isValidParenthesis(s2));
+        System.out.println(isValidParenthesis(s3));;
+    }
+}
