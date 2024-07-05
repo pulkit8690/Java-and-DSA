a9431cfc7eb4c9c7c2712388576cae4176a0b5ff
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-07-05 16:03:08 +0530

    CircularQueuesUsingArray

---
+++ Queue/CircularQueuesUsingArray.java
@@ -0,0 +1,79 @@
+public class CircularQueuesUsingArray {
+    public static class Queue{
+         int arr[];
+         int size;
+         int rear;
+         int front;
+        Queue(int n)
+        {
+            arr= new int[n];
+            size=n;
+            rear=-1;
+            front=-1;
+        }
+        public boolean isEmpty()
+        {
+            return rear==-1 && front==-1;
+        }
+        public boolean isFull()
+        {
+            return (rear+1)%size==front;
+        }
+        public void add(int data)
+        {
+            if(isFull())
+            {
+                System.out.println("Queue is Full");
+                return;
+            }
+            if(front==-1)
+            {
+                front=0;
+            }
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+        public int remove()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            int result=arr[front];
+            if(rear==front)
+            {
+                rear=front=-1;
+            }
+            else
+            {
+                front=(front+1)%size;
+            }
+            return result;
+        }
+        public int peek()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            return arr[front]; 
+        }
+    }
+    public static void main(String[] args) {
+        Queue q1 = new Queue(3);
+        q1.add(1);
+        q1.add(2);
+        q1.add(3);
+        System.out.println(q1.remove());
+        q1.add(4);
+        System.out.println(q1.remove());
+        q1.add(5);
+        while(!q1.isEmpty())
+        {
+            System.out.println(q1.peek());
+            q1.remove();
+        }
+    }
+}
