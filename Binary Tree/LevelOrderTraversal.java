23a4659a0047cdad5ba7fa1d0693e477ef8f481d
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-07-13 08:06:46 +0530

    HeightOfTree

--- Binary Tree/LevelOrderTraversal.java
+++ Binary Tree/LevelOrderTraversal.java
@@ -1,35 +1,35 @@
 import java.util.Queue;
 import java.util.LinkedList;
+
 public class LevelOrderTraversal {
-    public static class Node{
+    public static class Node {
         int data;
         Node left;
         Node right;
-        public Node(int data)
-        {
-            this.data=data;
-            this.left=null;
-            this.right=null;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
-    public static class BinaryTree{
-        static int idx=-1;
-        public  Node BuildTree(int nodes[])
-        {
+
+    public static class BinaryTree {
+        static int idx = -1;
+
+        public Node BuildTree(int nodes[]) {
             idx++;
-            if(nodes[idx]==-1)
-            {
+            if (nodes[idx] == -1) {
                 return null;
             }
             Node newNode = new Node(nodes[idx]);
-            newNode.left=BuildTree(nodes);
-            newNode.right=BuildTree(nodes);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
             return newNode;
         }
-        public void LevelOrder(Node root)
-        {
-            if(root==null)
-            {
+
+        public void LevelOrder(Node root) {
+            if (root == null) {
                 return;
             }
             Queue<Node> q = new LinkedList<>();
@@ -37,38 +37,32 @@ public class LevelOrderTraversal {
             q.add(null);
             while (!q.isEmpty()) {
                 Node currNode = q.remove();
-                if(currNode==null)
-                {
+                if (currNode == null) {
                     System.out.println();
-                    if(q.isEmpty())
-                    {
+                    if (q.isEmpty()) {
                         break;
-                    }
-                    else{
+                    } else {
                         q.add(null);
                     }
-                }
-                else
-                {
-                    System.out.print(currNode.data+" ");
-                    if(currNode.left!=null)
-                    {
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
                         q.add(currNode.left);
                     }
-                    if(currNode.right!=null)
-                    {
+                    if (currNode.right != null) {
                         q.add(currNode.right);
                     }
                 }
             }
-            
+
         }
 
     }
+
     public static void main(String[] args) {
-        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BinaryTree tree= new BinaryTree();
-        Node root =tree.BuildTree(nodes);
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.BuildTree(nodes);
         tree.LevelOrder(root);
     }
 }
