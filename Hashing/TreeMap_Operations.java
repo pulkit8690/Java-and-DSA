b7d8a5bb240a2b0fa274b0deb50880b6146f6c2c
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-07-22 15:58:31 +0530

    TreeMap_Operations

---
+++ Hashing/TreeMap_Operations.java
@@ -0,0 +1,25 @@
+//Keys Are Sorted
+// Red Black trees are used for its Implementation
+// Red Black trees are BST trees (Self Balancing trees)
+import java.util.*;
+public class TreeMap_Operations {
+    public static void main(String[] args) {
+        TreeMap<String,Integer> map = new TreeMap<>();
+        map.put("China",150);
+        map.put("India", 100);
+        map.put("USA", 50);
+        map.put("Pakistan", 25);
+        map.put("Nepal",2);
+        System.out.println(map);
+        //get
+        System.out.println(map.get("USA"));
+        System.out.println(map.get("UAE"));
+        //remove
+        map.remove("Pakistan");
+        System.out.println(map);
+        System.out.println(map.isEmpty());
+        //clear
+        map.clear();
+        System.out.println(map.isEmpty());
+    }
+}
