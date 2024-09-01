10abc39b7270224b666de77219513f567abbf0da
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-09-01 23:14:10 +0530

    ClimbingStairs_SpaceOptimize

---
+++ Dynamic Programming 1/ClimbingStairs_SpaceOptimize.java
@@ -0,0 +1,23 @@
+
+// Space Optimization O(1)
+public class ClimbingStairs_SpaceOptimize {
+    public static int count(int n)
+    {
+        if(n==0) return 1;
+        if(n==1) return 1;
+        int prev1=1;
+        int prev2=1;
+        int curr=0;
+        for(int i=2;i<=n;i++)
+        {
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+	public static void main(String[] args) {
+	    int n=5;
+		System.out.println(count(n));
+	}
+}
